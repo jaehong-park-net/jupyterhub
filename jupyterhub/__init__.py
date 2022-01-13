fe63e6e2fbcd3c937a9cbd621503c2f831c517dd
j.park <j.park@nexr.com>
2022-01-13 22:33:07 +0900

    tmp
    Signed-off-by: j.park <j.park@nexr.com>

--- jupyterhub/__init__.py
+++ jupyterhub/__init__.py
@@ -1,2 +1,7 @@
 from ._version import __version__
 from ._version import version_info
+
+from opentelemetry.instrumentation.tornado import TornadoInstrumentor
+
+
+TornadoInstrumentor().instrument()
